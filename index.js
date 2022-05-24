12fe76a2f762f933fcff0fc787ed8d3efa140e34
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2022-05-24 12:02:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var WhatCity = {}
 
+//Africa
 WhatCity["Egypt"] = ["Alexandria", "Sharm el-Sheikh", "Hurghada", "Cairo", "Aswan"];
 WhatCity["Algeria"] = ["Algiers", "Oran", "Constantine", "Batna", "Djelfa"];
 WhatCity["Morocco"] = ["Asilah", "Larache", "Tinghir", "El Jadida", "Chefchaouen"];
@@ -13,6 +14,8 @@ WhatCity["Yemen"] = ["Sana'a", "Ta'izz", "Al Hudaydah", "Aden", "Ibb"];
 WhatCity["Oman"] = ["Muscat", "Salalah", "Seeb", "Sohar", "Nizwa"];
 WhatCity["Tunisia"] = ["Kairouan", "Hammamet", "Sfax", "Dougga", "Sousse"];
 WhatCity["Mauritania"] = ["Nouakchott", "Nouadhibou", "Kiffa"];
+
+//Europe
 WhatCity["Iraq"] = ["Baghdad", "Mosul", "Basra", "Nasiriyah", "Hillah"];
 WhatCity["Qatar"] = ["Doha", "Al Rayyan", "Umm Salal Muhammad", "Al Wakrah"];
 WhatCity["Italy"] = ["Rome", "Venice", "Amalfi Coast", "Florence"];
@@ -31,14 +34,57 @@ WhatCity["Ireland"] = ["Dublin", "Galway", "Belfast", "Limerick", "Athlone"];
 WhatCity["Sweden"] = ["Monte Carlo", "La Condamine", "La Turbie", "Gorbio", "Saint-Jean-Cap-Ferrat"];
 WhatCity["Luxembourg"] = ["Clervaux", "Diekirch", "Larochette", "Remich", "Wiltz"];
 
-function RecommendDestination(country, city){
-    if(city === undefined && WhatCity[country] !== undefined ){
+// Asia
+WhatCity["China"] = ["Guilin", "Chengdu", "Beijing", "Xi'an", "Lhasa", "Shanghai"];
+WhatCity["India"] = ["Agra", "Goa", "Manali", "Mumbai", "Jaipur"];
+WhatCity["Indonesia"] = ["Yogyakarta", "Ubud", "Kuta", "Jakarta", "Bandung"];
+WhatCity["Pakistan"] = ["Islamabad", "Karachi", "Lahore", "Multan"];
+WhatCity["Bangladesh"] = ["Dhaka", "Sylhet", "St. Martin Island", "Sonargaon"];
+WhatCity["Japan"] = ["Tokyo", "Kyoto", "Hiroshima", "Kanazawa", "Fukuoka"];
+WhatCity["Philippines"] = ["Boracay", "Boracay", "Cebu", "Banaue", "Manila"];
+WhatCity["Vietnam"] = ["Hoi An", "Hanoi", "Da Nang", "Hue", "Da Lat"];
+WhatCity["Turkey"] = ["Antalya", "Istanbul", "Bursa", "Şanlıurfa", "Fethiye"];
+WhatCity["Thailand"] = ["Chiang Mai", "Bangkok", "Ayutthaya", "Chiang Rai", "Kanchanaburi"];
+WhatCity["South Korea"] = ["Incheon", "Gangneung", "Seogwipo", "Seoul", "Busan"];
+WhatCity["Malaysia"] = ["Kuala Lumpur", "Malacca", "Ipoh", "Kota Kinabalu", "Kuching"];
+WhatCity["Nepal"] = ["Kathmandu", "Janakpur", "Pokhara"];
+WhatCity["Sri Lanka"] = ["Galle", "Kandy", "Batticaloa", "Colombo", "Negombo"];
+WhatCity["Singapore"] = ["Yishun", "Butik Batok", "Pulao Ujong", "Tampines", "Jurong East"];
+WhatCity["Armenia"] = ["Yerevan", "Gyumri", "Vanadzor", "Goris"];
+WhatCity["Cyprus"] = ["Famagusta", "Nicosia", "Protaras", "Kyrenia", "Larnaca"];
+WhatCity["Maldives"] = ["Male", "Fuvahmulah", "Hulhumale", "Maafushi Island"];
+WhatCity["Brunei"] = ["Gadong", "Luagan Lalak", "Kampong Ayer", "Kota Batu", "Serasa"];
+
+// South America
+WhatCity["Brazil"] = ["Rio de Janeiro", "São Paulo", "Brasília", "Natal", "Fortaleza"];
+WhatCity["Colombia"] = ["Cartagena", "Bogota", "Medellin", "Cali", "Santa Marta"];
+WhatCity["Argentina"] = ["Buenos Aires", "Mendoza", "Ushuaia", "Córdoba", "Rosario"];
+WhatCity["Peru"] = ["Cuzco", "Lima", "Arequipa", "Ayacucho", "Puno"];
+WhatCity["Venezuela"] = ["Caracas", "Porlamar", "Merida", "Barquisimeto", "Carupano"];
+WhatCity["Chile"] = ["Santiago", "Valparaíso", "Pucón", "Castro", "San Pedro de Atacama"];
+WhatCity["Ecuador"] = ["Quito", "Cuenca", "Guayaquil", "Banos de Agua Santa", "Salinas"];
+WhatCity["Bolivia"] = ["La Paz", "Sucre", "Santa Cruz", "Copacabana", "Cochabamba"];
+WhatCity["Paraguay"] = ["Asuncion", "Encarnación", "San Lorenzo", "Capiata", "Lambaré"];
+WhatCity["Uruguay"] = ["Montevideo", "Colonia del Sacramento", "Salto", "Carmelo", "Laguna Garzon"];
+WhatCity["Guyana"] = ["Linden", "Georgetown", "New Amsterdam", "Anna Regina", "Bartica"];
+WhatCity["Suriname"] = ["Paramaribo", "Lelydorp", "Brokopondo", "Nieuw Nickerie", "Pokigron"];
+
+function RecommendCity(country){
+    if(WhatCity[country] !== undefined ){
         var random_index = Math.floor(Math.random() * (WhatCity[country].length -2));
         result = WhatCity[country]
         return result[random_index]
-    }else{
+    }
+}
+
+function RecommendPlaceInTheCity(country, city){
+    if(country !== undefined && city !== undefined){
 
     }
 }
 
-module.export = {RecommendDestination}
+
+console.log(RecommendCity("Egypt"))
+
+module.export = {RecommendCity, RecommendPlaceInTheCity}
+
